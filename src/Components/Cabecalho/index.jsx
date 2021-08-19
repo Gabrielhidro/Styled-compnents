82532dd2b777c528f27861610894b089cddb137f
gabrielhidro <gabrielhidro.dev@gmail.com>
2021-08-19 14:20:02 -0300

    Passed props in styled components

--- src/Components/Cabecalho/index.jsx
+++ src/Components/Cabecalho/index.jsx
@@ -1,20 +1,3 @@
-import React from "react";
-import bank_logo from "../../assets/images/bank_logo.svg";
+import CabecalhoPresentational from './presentational'
 
-const Cabecalho = () => {
-  return (
-    <div className="cabecalho">
-      <img className="imagem-logo" src={bank_logo} alt="Logo Smart Bank" />
-      <div>
-        <a className="btn-secundario" href="https://google.com">
-          Ajuda
-        </a>
-        <a className="btn-primario" href="https://google.com">
-          Sair
-        </a>
-      </div>
-    </div>
-  );
-};
-
-export default Cabecalho;
+export default CabecalhoPresentational
---
+++ src/Components/Cabecalho/styles.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import { corPrimaria } from "../UI/variables";
+
+export const StyledHeader = styled.nav`
+  background-color: ${corPrimaria};
+  display: flex;
+  justify-content: space-between;
+  padding: 0 15vw;
+  height: 10vh;
+  align-items: center;
+`
+
+export const Logo = styled.img`
+  height: 50px;
+  width: 50px;
+`
+
+export const BtnCabecalho = styled.a`
+  text-align: center;
+  border-radius: 3px;
+  padding: 5px 20px;
+  margin: 0 10px;
+  font-weight: 600;
+  border: 2px solid white;
+
+  background: ${(props) => props.primary ? "white" : corPrimaria};
+  color: ${(props) => props.primary ? corPrimaria : 'white'};
+` 
